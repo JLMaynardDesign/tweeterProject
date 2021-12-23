db1d895e9f70b00853aa99cd6e234389c307ce9d
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-12-23 03:50:39 -0500

    implement character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
-  // --- our code goes here ---
-  
+$(document).ready(function () {
+
+  $("tweet-text").on("input", function () {
+    const $counter = $(this).next().children(".counter");
+    const charsRemaining = 140 - this.value.length;
+
+    //adjust html of $counter when encountering input of characters
+    $counter.html(charsRemaining);
+
+    //adjust the color to red, when numbers of character exceed the limit
+    if (charsRemaining < 0) {
+
+      //.addClass() adds the specified class to each element in the set of matched elements
+      $counter.addClass("red");
+    } else {
+      $counter.removeClass("red");
+    }
+  });
 });
